b9878c69e569238d581e3189ec2e39fcb4bd3729
OlenaDiahil <122643832+OlenaDiahil@users.noreply.github.com>
2023-09-03 22:34:33 +0300

    Initial commit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Filter = ({ value, onChangeFilterValue }) => {
+  return (
+    <div>
+      <input onChange={onChangeFilterValue} value={value}></input>
+    </div>
+  );
+};
+export default Filter;
+
