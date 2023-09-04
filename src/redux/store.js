03dbd24318a0477dcf4cbba3519ff5e44e939870
OlenaDiahil <122643832+OlenaDiahil@users.noreply.github.com>
2023-09-05 00:00:47 +0300

    blacklist

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,8 @@ import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
   key: 'root',
-  storage,
+    storage,
+    blacklist: ['filter'],
 }
 
 const rootReducer = combineReducers({
