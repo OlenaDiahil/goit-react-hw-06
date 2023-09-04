f53e2b4c3f338f98fe55c026806e7810f54823cd
OlenaDiahil <122643832+OlenaDiahil@users.noreply.github.com>
2023-09-04 23:57:00 +0300

    del

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -32,14 +32,3 @@ const ContactList = () => {
 };
 
 export default ContactList;
-
-ContactList.propTypes = {
-  paramList: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  onDelete: PropTypes.func,
-};
